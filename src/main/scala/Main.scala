551940e1cb80e5606895d2e849d163f11c247877
f_kurabayashi <f_kurabayashi@RN0831.local>
2018-11-26 18:50:28 +0900

    本番環境動作確認

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,57 +2,58 @@
 import io.finch._
 import io.finch.circe._
 import io.finch.syntax._
+import cats.implicits._
+import cats.data.NonEmptyList
 import io.circe
 import io.circe.syntax._
 import io.circe.parser._
 import io.circe.generic.auto._
 import com.twitter.util._
 import com.twitter.util.Await
+import com.twitter.util.FuturePool
 import com.twitter.conversions.time._
 import com.twitter.finagle.{Http, Service, http}
 import com.twitter.finagle.http.{Request, Response}
 import Model._
 
 object Main extends App {
-  val dsp = Seq("127.0.0.1")
+  val dsp: NonEmptyList[String] = NonEmptyList.of("10.100.100.20","10.100.100.22", "10.100.100.24")
+  val servicePool: NonEmptyList[Service[Request, Response]] = createClient(dsp)
 
-  //Connection Pooling
-  val servicePool: Seq[Service[Request, Response]] = createClient(dsp)
-
-  //curl http://localhost:8081/index -X POST -H "Content-Type: application/json" -d '{"app_id": 9999}'
+  //Example curl http://localhost:8081/index -X POST -H "Content-Type: application/json" -d '{"app_id": 9999}'
   val index: Endpoint[ResponseToSdk] =
     post("index" :: jsonBody[RequestFromSdk]) { request: RequestFromSdk =>
-
       val requestDspParams: String = RequestToDsp(request.app_id).asJson.toString
-      val listOfFutures: Seq[Future[Either[circe.Error, ResponseFromDsp]]] =
-        servicePool.par.map { x =>
+      val listOfFutures: NonEmptyList[Future[Either[circe.Error, ResponseFromDsp]]] =
+        servicePool.map { x =>
           dspRequest(x, requestDspParams, "/req")
-            .map(responseCheck)}.seq
+            .map(responseCheck)}
 
-      Future.collect(listOfFutures)
-        .map { x =>
-          val dspResList: Seq[ResponseFromDsp] = x.map(_.right.get)
-          val winIndex = searchWinIndex(dspResList)
-          val winDsp = dspResList(winIndex)
-          val secondPrice: Float = calcSecondPrice(dspResList, winIndex)
-          val win = WinNotice(winDsp.request_id, secondPrice)
-          dspRequest(servicePool(winIndex), win.toString, "/win")
-          //WriteLog.write(win)
-          Ok(ResponseToSdk(winDsp.url))}
-    }
+      Future.collect(listOfFutures.toList)
+        .flatMap { x =>
+          FuturePool.unboundedPool{
+            val nel: NonEmptyList[Either[circe.Error, ResponseFromDsp]] = NonEmptyList.fromListUnsafe(x.toList)
+            println(nel)
+            val dspResList: NonEmptyList[(ResponseFromDsp, Int)] = nel.map(_.right.get).zipWithIndex
+            val winDsp: (ResponseFromDsp, Int) = searchWinDsp(dspResList)
+            val secondPrice: Float = calcSecondPrice(dspResList, winDsp)
+            val win = WinNotice(winDsp._1.request_id, secondPrice)
+            dspRequest(servicePool.toList(winDsp._2), win.toString, "/win")
+            //WriteLog.write(win)
+            Ok(ResponseToSdk(winDsp._1.url))}}}
+
     val server = Http.server.serve(":8081", index.toService)
     Await.ready(server)
 
-  def createClient(dspSeq: Seq[String]): Seq[Service[Request, Response]] ={
-    val poolSeq: Seq[Service[Request, Response]] =
+  def createClient(dspSeq: NonEmptyList[String]): NonEmptyList[Service[Request, Response]] ={
+    val poolSeq: NonEmptyList[Service[Request, Response]] =
       dsp.map{i =>
         val client: Service[Request, Response] =
           Http.client
             .withRequestTimeout(100.millis)
             .newService(s"$i:8082")
         client}
-    poolSeq
-  }
+    poolSeq}
 
   def dspRequest(client: Service[Request, Response], requestContent: String, reqApi: String): Future[Option[Response]] ={
     val request: Request = http.Request(http.Method.Post, reqApi).host("8082")
@@ -60,23 +61,21 @@ object Main extends App {
     request.setContentString(requestContent)
     client(request)
       .map(Some(_): Option[Response])
-      .handle{case _ => None}
-  }
+      .handle{case _ => None}}
 
-  def responseCheck(res: Option[Response]): Either[circe.Error, ResponseFromDsp] ={
+  def responseCheck(res: Option[Response]): Either[circe.Error, ResponseFromDsp] =
     res match {
       case Some(value) => decode[ResponseFromDsp](value.getContentString)
-      case None => Right(ResponseFromDsp("-1", "-1", 0))
-    }
-  }
+      case None => Right(ResponseFromDsp("-1", "-1", 0))}
 
-  def searchWinIndex(list: Seq[ResponseFromDsp]): Int ={
-    val winIndex: Int = list.indexWhere(_.price == list.map(_.price).max)
-    winIndex
-  }
+  def searchWinDsp(nel: NonEmptyList[(ResponseFromDsp, Int)]): (ResponseFromDsp, Int) ={
+    val max: Float = nel.map(x => x._1.price).sorted.reverse.head
+    val winIndex: (ResponseFromDsp, Int) =
+      nel.find(t => t._1.price == max).get
+    winIndex}
 
-  def calcSecondPrice(seq: Seq[ResponseFromDsp], winIndex: Int): Float ={
-    if(seq.length <= 1) seq.head.price + 1
-    else seq.drop(winIndex).map(_.price).max
-  }
+  def calcSecondPrice(seq: NonEmptyList[(ResponseFromDsp, Int)], winDsp: (ResponseFromDsp, Int)): Float =
+    if(seq.length <= 1) 1
+    else seq.filterNot(x => x == winDsp).map(_._1.price).sorted.reverse.head
+
 }
